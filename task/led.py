e99261608ff36715b0d976ad2a72062cf15c0ba3
张杰 <2770099900@qq.com>
2023-07-08 16:26:17 +0800

    task,base,led

--- task/led.py
+++ task/led.py
@@ -8,4 +8,34 @@ def blue_led():
     go.set_rgb_light(number="LED_1", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
-    go.set_rgb_light(number="LED_4", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1)
+def red_led():
+    go.set_rgb_light(number="LED_1", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_2", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_3", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1)
+def green_led():
+    go.set_rgb_light(number="LED_1", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_2", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_3", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1)
+def yellow_led():
+    go.set_rgb_light(number="LED_1", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_2", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_3", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1)
+def purple_led():
+    go.set_rgb_light(number="LED_1", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_2", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_3", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1)
+def cyan_led():
+    go.set_rgb_light(number="LED_1", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_2", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_3", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1)
+def white_led():
+    go.set_rgb_light(number="LED_1", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_2", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_3", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1) 
+    go.set_rgb_light(number="LED_4", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1)
--- task/task.py
+++ task/task.py
@@ -1,10 +1,30 @@
-# version: Python3
 from DobotEDU import *
+import led
 class task:
-    def ctf(text):
-        object_class=text
-        beta_go.grab_obj_cartofloor(object_class)
-    def ftc(text):
-        object_class=text
-        beta_go.grab_obj_cartofloor(object_class)
-    
+    def get_power():#电源
+        led.white_led()
+        pwv=go.get_power_voltage(self)
+        return pwv
+    def pgotop():#点到点
+        led.white_led()
+        go.set_ptp_with_l(self, x=250, y=0, z=0, r=0, mode=0)
+    def findpet():
+        go.set_running_mode(1)
+        led.white_led()
+        voice("开始寻找宠物")
+        go.set_move_dist(45, 45, 2, 2)
+        voice("未检测到宠物")
+        go.set_move_dist(-45, 45, 2, 2)
+        voice("未检测到宠物")
+        go.set_move_dist(-45, -45, 2, 2)
+        voice("未检测到宠物")
+        go.set_move_dist(45, -45, 2, 2)
+    def playcat():
+        led.white_led()
+        beta_go.grab_obj(self, 1)
+        voice("喵 喵喵")
+        ###
+    def 
+
+
+
