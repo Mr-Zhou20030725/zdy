267a8a35c3535fe857dfa163fcc02479d3324949
Mr-Zhou20030725 <95691418+Mr-Zhou20030725@users.noreply.github.com>
2023-07-08 13:27:22 +0800

    init

---
+++ task/task.py
@@ -0,0 +1,10 @@
+# version: Python3
+from DobotEDU import *
+class task:
+    def ctf(text):
+        object_class=text
+        beta_go.grab_obj_cartofloor(object_class)
+    def ftc(text):
+        object_class=text
+        beta_go.grab_obj_cartofloor(object_class)
+    
