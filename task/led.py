5d3398495fca4463f3e0675e313c18a8aa36862e
张杰 <2770099900@qq.com>
2023-07-10 19:14:31 +0800

    int

--- task/led.py
+++ task/led.py
@@ -1,41 +1,42 @@
 from DobotEDU import *
-def breath_led():#呼吸灯
+def set_breath_led():#呼吸灯
     go.set_rgb_light(number="LED_1", effect=4, r=100 ,g=0, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=4, r=100 ,g=0, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=4, r=100 ,g=0, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=4, r=100 ,g=0, b=0, cycle=0.5, counts=1) 
-def blue_led():
+def set_blue_led():
     go.set_rgb_light(number="LED_1", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=0,g=0, b=255, cycle=0.5, counts=1)
-def red_led():
+def set_red_led():
     go.set_rgb_light(number="LED_1", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=255,g=0, b=0, cycle=0.5, counts=1)
-def green_led():
+def set_green_led():
     go.set_rgb_light(number="LED_1", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=0,g=255, b=0, cycle=0.5, counts=1)
-def yellow_led():
+def set_yellow_led():
     go.set_rgb_light(number="LED_1", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=255,g=255, b=0, cycle=0.5, counts=1)
-def purple_led():
+def set_purple_led():
     go.set_rgb_light(number="LED_1", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=255,g=0, b=255, cycle=0.5, counts=1)
-def cyan_led():
+def set_cyan_led():
     go.set_rgb_light(number="LED_1", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=0,g=255, b=255, cycle=0.5, counts=1)
-def white_led():
+def set_white_led():
     go.set_rgb_light(number="LED_1", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_2", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_3", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1) 
     go.set_rgb_light(number="LED_4", effect=0, r=255,g=255, b=255, cycle=0.5, counts=1)
+
